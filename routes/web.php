8538629fdb8e69efe31fe2555e2cb4779a904937
hadiprasetiyo <hadiiyok01@gmail.com>
2024-05-10 17:41:57 +0800

    feat: Add routes for Instructor section
    Added routes for Instructor section:
    - '/dashboard_instruktur': Menampilkan dashboard instruktur.
    - '/form_sekolah': Menampilkan formulir untuk sekolah.
    - '/form_kelas': Menampilkan formulir untuk kelas.

--- routes/web.php
+++ routes/web.php
@@ -1,14 +1,6 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\siswaController;
-use App\Http\Controllers\instrukturController;
-use App\Http\Controllers\sekolahController;
-use App\Http\Controllers\nilai_raportController;
-use App\Http\Controllers\DashboardController;
-
-
-
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -27,23 +19,15 @@ Route::get('/', function () {
 Route::get('/home', function () {
     return view('Landingpage.home');
 });
-// ------- Admin ------//
-Route::get('/admin', function () {
-    return view('backend.dashboard');
-});
 
-Route::get('/sekolah', function () {
-    return view('backend.index');
-});
 
-// Route::resource('/siswa', siswaController::class);
-// Route::resource('/sekolah', sekolahController::class);
-// Route::resource('/instruktur', instrukturController::class);
-// Route::resource('/nilai_raport', nilai_raportController::class);
-
-Route::get('/profile', function () {
-    return view('backend.profile');
+// --------- Instruktur --------
+Route::get('/dashboard_instruktur', function () {
+    return view('Instruktur.dashboard_instruktur');
 });
-Route::get('/contact', function () {
-    return view('backend.contact');
+Route::get('/form_sekolah', function () {
+    return view('Instruktur.form_sekolah');
+});
+Route::get('/form_kelas', function () {
+    return view('Instruktur.form_kelas');
 });
